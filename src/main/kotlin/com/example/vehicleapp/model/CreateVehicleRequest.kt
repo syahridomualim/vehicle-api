c40910302bc79a79541abb159026a5d23681c778
Mu'alim Syahrido <IDS.MUALIMS@xl.co.id>
2023-01-10 21:06:54 +0700

    fixing validation

--- src/main/kotlin/com/example/vehicleapp/model/CreateVehicleRequest.kt
+++ src/main/kotlin/com/example/vehicleapp/model/CreateVehicleRequest.kt
@@ -1,25 +1,23 @@
-package com.example.vehicleapp.model
+    package com.example.vehicleapp.model
 
-import com.example.vehicleapp.entity.Color
+    import com.example.vehicleapp.entity.Color
 import com.example.vehicleapp.entity.Fuel
-import com.example.vehicleapp.validation.ColorValidation
 import jakarta.validation.constraints.NotEmpty
 import jakarta.validation.constraints.Pattern
 import jakarta.validation.constraints.Size
 
 data class CreateVehicleRequest(
-    @field:NotEmpty(message = "Please provide your tag number")
+    @field:Pattern(regexp = "^[A-Z]{2} [0-9]{4} [A-Z]{1,3}$", message = "Please provide your tag number")
     val tagNumber: String,
     @field:Size(min = 5, message = "Please provide the name minimum of 5 characters")
     val owner: String,
     @field:NotEmpty(message = "Please provide the brand")
     val brand: String,
     @field:NotEmpty(message = "Please provide the year production")
-    @field:Pattern(regexp = "^\\\\d{4}\$")
+    @field:Pattern(regexp = "^\\d{4}$")
     val year: String,
     @field:Size(min = 1, message = "Please provide cylinder capacity")
     val cylinderCapacity: String,
     val fuel: Fuel,
-    @field:ColorValidation(message = "Please provide your color")
     val color: Color
 )
